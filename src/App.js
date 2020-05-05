bd0b08fe300879589872246d88c067bd575c0d2b
Felipe Sousa <felipzbr@gmail.com>
2020-05-05 02:42:31 -0400

    [initial]: initial commit

--- src/App.js
+++ src/App.js
@@ -1,26 +1,47 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { useState, useEffect } from 'react';
+import { Subject, merge } from 'rxjs';
+import { debounceTime } from 'rxjs/operators';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+const city$ = new Subject();
+const state$ = new Subject();
+
+const App = () => {
+  const [value] = useState(null);
+  const [data, setData] = useState([]);
+
+  useEffect(() => {
+    merge(city$, state$)
+      .pipe(debounceTime(5000))
+      .subscribe(fetchData);
+  }, [value])
+
+  const onCity = (e) => {
+    city$.next({ city: e.target.value });
+  }
+
+  const onState = (e) => {
+    state$.next({ state: e.target.value });
+  }
+
+  const fetchData = async ({ city = '', state = '' }) => {
+    try {
+      const response = await fetch(`https://usa-cities-api.herokuapp.com/cities?city=${city}&state=${state}`);
+      const data = await response.json();
+
+      setData(data);
+    } catch (e) {
+      console.error(e.toString());
+    }
+  }
+
+  return <>
+    <input type="text" placeholder="city name here" id="city" onChange={onCity} />
+    <input type="text" placeholder="state name here" id="state" onChange={onState} />
+    <p>{value}</p>
+    <ul>
+      { data && data.map(({ city, state }) => <li>{city} - {state}</li>) }
+    </ul>
+  </>;
 }
 
 export default App;
